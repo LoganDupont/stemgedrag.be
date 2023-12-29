26ad95d21a8a977feda2b1ad60789b12d47d8226
Logan Dupont <l.dupont+github@hotmail.be>
2023-12-29 17:17:45 +0100

    feat(shell): Add responsive sidenav (#43)

--- stemgedrag/apps/stemgedrag/src/app/app.config.ts
+++ stemgedrag/apps/stemgedrag/src/app/app.config.ts
@@ -1,13 +1,16 @@
-import { ApplicationConfig } from '@angular/core';
 import { provideHttpClient } from '@angular/common/http';
+import { ApplicationConfig } from '@angular/core';
+import { provideAnimations } from '@angular/platform-browser/animations';
 import {
   provideRouter,
   withEnabledBlockingInitialNavigation,
 } from '@angular/router';
 import { appRoutes } from './app.routes';
+
 export const appConfig: ApplicationConfig = {
   providers: [
     provideHttpClient(),
     provideRouter(appRoutes, withEnabledBlockingInitialNavigation()),
+    provideAnimations(),
   ],
 };
--- stemgedrag/libs/stemgedrag/feat-shell/src/lib/stemgedrag-feat-shell/stemgedrag-feat-shell.component.ts
+++ stemgedrag/libs/stemgedrag/feat-shell/src/lib/stemgedrag-feat-shell/stemgedrag-feat-shell.component.ts
@@ -1,14 +1,36 @@
-import { Component } from '@angular/core';
+import { BreakpointObserver, Breakpoints } from '@angular/cdk/layout';
 import { CommonModule } from '@angular/common';
-import { MatToolbarModule } from '@angular/material/toolbar';
+import { Component, inject } from '@angular/core';
 import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
+import { MatListModule } from '@angular/material/list';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatToolbarModule } from '@angular/material/toolbar';
 import { RouterModule } from '@angular/router';
+import { Observable, map, shareReplay } from 'rxjs';
 
 @Component({
   selector: 'sg-stemgedrag-feat-shell',
   standalone: true,
-  imports: [CommonModule, RouterModule, MatToolbarModule, MatButtonModule],
+  imports: [
+    CommonModule,
+    RouterModule,
+    MatToolbarModule,
+    MatButtonModule,
+    MatSidenavModule,
+    MatListModule,
+    MatIconModule,
+  ],
   templateUrl: './stemgedrag-feat-shell.component.html',
   styleUrls: ['./stemgedrag-feat-shell.component.scss'],
 })
-export class StemgedragFeatShellComponent {}
+export class StemgedragFeatShellComponent {
+  private breakpointObserver = inject(BreakpointObserver);
+
+  isHandset$: Observable<boolean> = this.breakpointObserver
+    .observe(Breakpoints.Handset)
+    .pipe(
+      map((result) => result.matches),
+      shareReplay()
+    );
+}
