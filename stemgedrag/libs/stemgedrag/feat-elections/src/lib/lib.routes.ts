5f286b7fcb6945f89bcafa1b763b9abbf7d81c4b
Logan Dupont <l.dupont+github@hotmail.be>
2024-03-31 23:04:38 +0200

    feat(elections): Add election program page (#59)

---
+++ stemgedrag/libs/stemgedrag/feat-elections/src/lib/lib.routes.ts
@@ -0,0 +1,6 @@
+import { Route } from '@angular/router';
+import { ElectionProgrammeOverviewSmartComponent } from './smart-components/election-programme-overview/election-programme-overview.smart-component';
+
+export const stemgedragFeatElectionsRoutes: Route[] = [
+  { path: '', component: ElectionProgrammeOverviewSmartComponent },
+];
