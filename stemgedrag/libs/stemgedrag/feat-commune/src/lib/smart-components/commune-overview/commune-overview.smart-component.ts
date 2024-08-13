619f886d53639730f8532310d936ea3ba1dfa178
Logan Dupont <l.dupont+github@hotmail.be>
2024-08-13 12:36:48 +0200

    feat(commune): Add search to filter list

--- stemgedrag/libs/stemgedrag/feat-commune/src/lib/smart-components/commune-overview/commune-overview.smart-component.ts
+++ stemgedrag/libs/stemgedrag/feat-commune/src/lib/smart-components/commune-overview/commune-overview.smart-component.ts
@@ -1,21 +1,44 @@
 import { CommonModule } from '@angular/common';
-import { Component, inject } from '@angular/core';
-import { map } from 'rxjs';
+import { Component, computed, inject, signal } from '@angular/core';
+import { toSignal } from '@angular/core/rxjs-interop';
+import { FormsModule } from '@angular/forms';
+import { MatButtonModule } from '@angular/material/button';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatIconModule } from '@angular/material/icon';
+import { MatInputModule } from '@angular/material/input';
+import { MatListModule } from '@angular/material/list';
 import { FacadeService } from '../../facade.service';
 
 @Component({
   selector: 'sg-commune-overview',
   standalone: true,
-  imports: [CommonModule],
+  imports: [
+    CommonModule,
+    MatFormFieldModule,
+    MatInputModule,
+    FormsModule,
+    MatButtonModule,
+    MatIconModule,
+    MatListModule,
+  ],
   templateUrl: './commune-overview.smart-component.html',
   styleUrls: ['./commune-overview.smart-component.scss'],
 })
 export class CommuneOverviewSmartComponent {
   private readonly facadeService = inject(FacadeService);
 
-  communes$ = this.facadeService.getCommunes();
+  searchValue = signal('');
+  communes = toSignal(this.facadeService.getCommunes(), { initialValue: [] });
 
-  sortedCommunes$ = this.communes$.pipe(
-    map((communes) => communes.sort((a, b) => a.name.localeCompare(b.name)))
-  );
+  filteredCommunes = computed(() => {
+    const searchValue = this.searchValue();
+    let filteredCommunes;
+    if (!searchValue) {
+      filteredCommunes = this.communes();
+    }
+    filteredCommunes = this.communes().filter((commune) =>
+      commune.name.toLowerCase().includes(searchValue.toLowerCase())
+    );
+    return filteredCommunes.sort((a, b) => a.name.localeCompare(b.name));
+  });
 }
