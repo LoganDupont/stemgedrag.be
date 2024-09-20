a88151feb5f8cd800e8023c1e705b3f95887b7a1
Logan Dupont <l.dupont+github@hotmail.be>
2024-09-20 10:06:32 +0200

    feat(elections): Enable navigation to election page (#86)

--- stemgedrag/libs/stemgedrag/feat-elections/src/lib/smart-components/election-programme-overview/election-programme-overview.smart-component.ts
+++ stemgedrag/libs/stemgedrag/feat-elections/src/lib/smart-components/election-programme-overview/election-programme-overview.smart-component.ts
@@ -9,70 +9,70 @@ import { Component } from '@angular/core';
   styleUrl: './election-programme-overview.smart-component.css',
 })
 export class ElectionProgrammeOverviewSmartComponent {
-  protected parties: any[] = [
-    {
-      name: 'Vooruit',
-      link: 'https://www.vooruit.org/programma-download',
-    },
-    {
-      name: 'PVDA',
-      link: 'https://www.pvda.be/programma',
-    },
-    {
-      name: 'Groen',
-      link: 'https://www.groen.be/programma',
-    },
-    {
-      name: 'CD&V',
-      link: 'https://www.cdenv.be/verkiezingen_2024',
-    },
-    {
-      name: 'Open VLD',
-      link: 'https://www.openvld.be/verkiezingsprogramma_open_vld',
-    },
-    {
-      name: 'N-VA',
-      link: 'https://www.n-va.be/',
-    },
-    {
-      name: 'Vlaams Belang',
-      link: 'https://www.vlaamsbelang.org/programma',
-    },
-    {
-      name: 'Voor U',
-      link: 'https://www.vooru.be/de-12-voor-u',
-    },
-    {
-      name: 'Blanco',
-      link: 'https://blanco2024.be/#voorstel',
-    },
-    {
-      name: 'Volt Europa',
-      link: 'https://voltbelgie.org/standpunten/beleidsgrondslagen/verkiezingsprogramma-2024',
-    },
-    {
-      name: 'BoerBurgerBelangen (BBB)',
-      link: 'https://boerburgerbelangen.vlaanderen/onze-standpunten/',
-    },
-    {
-      name: 'De Belgische Unie - Union Belge (B.U.B.)',
-      link: 'https://www.unionbelge.be/#program',
-    },
-    {
-      name: "L'Unie",
-      link: 'https://www.lunie.be/nl/vision',
-    },
-    {
-      name: 'Agora',
-      link: 'https://nl.agora.brussels/',
-    },
-    {
-      name: 'DierAnimal',
-      link: 'https://www.dieranimal.be/nl/home/ons-programma/',
-    },
-    {
-      name: 'Team Fouad Ahidar',
-      link: 'https://fouadahidar.com/nl/#programma',
-    },
-  ].sort((a, b) => a.name.localeCompare(b.name));
+  // protected parties: any[] = [
+  //   {
+  //     name: 'Vooruit',
+  //     link: 'https://www.vooruit.org/programma-download',
+  //   },
+  //   {
+  //     name: 'PVDA',
+  //     link: 'https://www.pvda.be/programma',
+  //   },
+  //   {
+  //     name: 'Groen',
+  //     link: 'https://www.groen.be/programma',
+  //   },
+  //   {
+  //     name: 'CD&V',
+  //     link: 'https://www.cdenv.be/verkiezingen_2024',
+  //   },
+  //   {
+  //     name: 'Open VLD',
+  //     link: 'https://www.openvld.be/verkiezingsprogramma_open_vld',
+  //   },
+  //   {
+  //     name: 'N-VA',
+  //     link: 'https://www.n-va.be/',
+  //   },
+  //   {
+  //     name: 'Vlaams Belang',
+  //     link: 'https://www.vlaamsbelang.org/programma',
+  //   },
+  //   {
+  //     name: 'Voor U',
+  //     link: 'https://www.vooru.be/de-12-voor-u',
+  //   },
+  //   {
+  //     name: 'Blanco',
+  //     link: 'https://blanco2024.be/#voorstel',
+  //   },
+  //   {
+  //     name: 'Volt Europa',
+  //     link: 'https://voltbelgie.org/standpunten/beleidsgrondslagen/verkiezingsprogramma-2024',
+  //   },
+  //   {
+  //     name: 'BoerBurgerBelangen (BBB)',
+  //     link: 'https://boerburgerbelangen.vlaanderen/onze-standpunten/',
+  //   },
+  //   {
+  //     name: 'De Belgische Unie - Union Belge (B.U.B.)',
+  //     link: 'https://www.unionbelge.be/#program',
+  //   },
+  //   {
+  //     name: "L'Unie",
+  //     link: 'https://www.lunie.be/nl/vision',
+  //   },
+  //   {
+  //     name: 'Agora',
+  //     link: 'https://nl.agora.brussels/',
+  //   },
+  //   {
+  //     name: 'DierAnimal',
+  //     link: 'https://www.dieranimal.be/nl/home/ons-programma/',
+  //   },
+  //   {
+  //     name: 'Team Fouad Ahidar',
+  //     link: 'https://fouadahidar.com/nl/#programma',
+  //   },
+  // ].sort((a, b) => a.name.localeCompare(b.name));
 }
