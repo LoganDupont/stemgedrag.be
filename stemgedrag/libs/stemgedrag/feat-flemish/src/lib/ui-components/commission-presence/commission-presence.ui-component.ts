10fe0f23b39768346e626f8d8048918f777d7f7b
Logan Dupont <l.dupont+github@hotmail.be>
2025-03-29 21:25:43 +0100

    feat(flemish): Add commission presence for chairman, added-member and not-member

--- stemgedrag/libs/stemgedrag/feat-flemish/src/lib/ui-components/commission-presence/commission-presence.ui-component.ts
+++ stemgedrag/libs/stemgedrag/feat-flemish/src/lib/ui-components/commission-presence/commission-presence.ui-component.ts
@@ -16,32 +16,57 @@ export class CommissionPresenceUiComponent {
   public commissionPresence = input.required<AanwezigheidCommissie>();
 
   protected commission = computed(() => this.commissionPresence().commissie);
+  protected presenceChairman = computed(
+    () => this.commissionPresence()['voorzitter-aanwezigheid']
+  );
   protected presencePermanentMember = computed(
     () => this.commissionPresence()['vast-lid-aanwezigheid']
   );
   protected presenceSubstituteMember = computed(
     () => this.commissionPresence()['plaatsvervangend-lid-aanwezigheid']
   );
+  protected presenceAddedMember = computed(
+    () => this.commissionPresence()['toegevoegd-lid-aanwezigheid']
+  );
+  protected presenceNotMember = computed(
+    () => this.commissionPresence()['niet-lid-aanwezigheid']
+  );
   private present = computed(
-    () =>
-      this.presencePermanentMember()?.aanwezig ||
-      0 + this.presenceSubstituteMember()?.aanwezig ||
-      0
+    () => {
+      let present = 0;
+      const { commissie, ...rest } = this.commissionPresence();
+      for (const presence of Object.values(rest)) {
+        present += presence.aanwezig || 0;
+      }
+      return present;
+    }
+    // (this.presencePermanentMember()?.aanwezig || 0) +
+    // (this.presenceSubstituteMember()?.aanwezig || 0)
   );
-  private apologized = computed(
-    () =>
-      this.presencePermanentMember()?.verontschuldigd ||
-      0 + this.presenceSubstituteMember()?.verontschuldigd ||
-      0
-  );
-  private absent = computed(
-    () =>
-      this.presencePermanentMember()?.afwezig ||
-      0 + this.presenceSubstituteMember()?.afwezig ||
-      0
-  );
+  // private apologized = computed(
+  //   () =>
+  //     this.presencePermanentMember()?.verontschuldigd ||
+  //     0 + this.presenceSubstituteMember()?.verontschuldigd ||
+  //     0
+  // );
+  // private absent = computed(
+  //   () =>
+  //     this.presencePermanentMember()?.afwezig ||
+  //     0 + this.presenceSubstituteMember()?.afwezig ||
+  //     0
+  // );
   private total = computed(
-    () => this.present() + this.apologized() + this.absent()
+    () => {
+      let total = 0;
+      const { commissie, ...rest } = this.commissionPresence();
+      for (const presence of Object.values(rest)) {
+        for (const amount of Object.values(presence)) {
+          total += amount || 0;
+        }
+      }
+      return total;
+    }
+    // this.present() + this.apologized() + this.absent()
   );
   protected presence = computed(() =>
     this.total() ? this.present() / this.total() : 0
--- stemgedrag/libs/stemgedrag/type-volks-vertegenwoordiger/src/lib/types/aanwezigheid-commissie.type.ts
+++ stemgedrag/libs/stemgedrag/type-volks-vertegenwoordiger/src/lib/types/aanwezigheid-commissie.type.ts
@@ -3,6 +3,9 @@ import { CommissieLI } from './commissie-list-item.type';
 
 export type AanwezigheidCommissie = Readonly<{
   commissie: CommissieLI;
+  'voorzitter-aanwezigheid': AanwezigheidCommissieAantal;
   'vast-lid-aanwezigheid': AanwezigheidCommissieAantal;
   'plaatsvervangend-lid-aanwezigheid': AanwezigheidCommissieAantal;
+  'toegevoegd-lid-aanwezigheid': AanwezigheidCommissieAantal;
+  'niet-lid-aanwezigheid': AanwezigheidCommissieAantal;
 }>;
